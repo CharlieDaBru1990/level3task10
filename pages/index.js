4907afa1571309f53770bf26496c5a8cb3694483
CharlieDaBru1990 <cgprins@gmail.com>
2022-09-20 19:43:27 +0200

    Fixed Google Analytics Again

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function gtag(){dataLayer.push(arguments);}
 
 const Index = () => (
     <div>
-   Script async src="https://www.googletagmanager.com/gtag/js?id=UA-241661607-1"/>
+   <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-241661607-1"/>
 <Script>
   {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
